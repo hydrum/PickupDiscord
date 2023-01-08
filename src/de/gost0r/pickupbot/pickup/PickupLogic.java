29b70418338edeaed1e22df8eaf0587e650160a4
Benoit Lery <hsbiddle@gmail.com>
2023-01-08 15:07:53 +0100

    Updated ban timers

--- src/de/gost0r/pickupbot/pickup/PickupLogic.java
+++ src/de/gost0r/pickupbot/pickup/PickupLogic.java
@@ -73,8 +73,8 @@ public class PickupLogic {
 		createCurrentMatches();
 		
 		banDuration = new HashMap<BanReason, String[]>();
-		banDuration.put(BanReason.NOSHOW, new String[] {"10m", "30m", "1h", "2h", "6h", "12h", "3d", "1w", "2w", "1M"});
-		banDuration.put(BanReason.RAGEQUIT, new String[] {"12h", "1d", "3d", "1w", "2w", "1M", "3M"});
+		banDuration.put(BanReason.NOSHOW, new String[] {"10m", "30m", "1h", "2h", "6h", "12h", "1d", "2d", "3d", "1w", "1w", "1w", "1M"});
+		banDuration.put(BanReason.RAGEQUIT, new String[] {"6h", "12h", "1d", "2d", "3d", "1w", "1w", "1w", "1w", "1w", "1w", "1w", "1M"});
 		
 		awaitingServer = new LinkedList<Match>();
 		
