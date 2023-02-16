56825c4a41d5a709fe7e9b088dbc614837eedbcd
Holycrap <gabachour44@gmail.com>
2023-02-16 14:08:26 -0500

    Indicate server region during the picks

--- src/de/gost0r/pickupbot/pickup/Config.java
+++ src/de/gost0r/pickupbot/pickup/Config.java
@@ -226,6 +226,7 @@ public class Config {
 	public static final String pkup_go_pub_pick 		= ".captain. pick a player:";
 	public static final String pkup_go_pub_pickjoin		= ".pick. joins the **.color.** team.";
 	public static final String pkup_go_pub_servspawn	= "**Spawning Server:** .flag. ``.city.`` (fairest server to all players)\n:hourglass: The ip will be send to all players when the server is spawned.";
+	public static final String pkup_go_pub_requestserver= "**Requesting Server:** .flag. ``.region.``";
 
 	public static final String pkup_aftermath_head		= "**.gametype.**: Aftermath #.gamenumber. (.map.):";
 	public static final String pkup_aftermath_result	= ".team. .result. (.score.) -";
