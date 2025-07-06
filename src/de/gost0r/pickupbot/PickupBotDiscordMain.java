0b454a2caaae49e3435a4358614bdb238dbe9927
hydrum <gost0r@yahoo.de>
2025-07-06 12:04:23 +0200

    conditionally require .prod.env

--- src/de/gost0r/pickupbot/PickupBotDiscordMain.java
+++ src/de/gost0r/pickupbot/PickupBotDiscordMain.java
@@ -1,79 +1,74 @@
 package de.gost0r.pickupbot;
 
-import java.io.IOException;
-import java.util.Locale;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.FileHandler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
-
-import de.gost0r.pickupbot.ftwgl.FtwglAPI;
-import io.github.cdimascio.dotenv.Dotenv;
-import org.json.JSONException;
-
 import de.gost0r.pickupbot.discord.DiscordBot;
+import de.gost0r.pickupbot.ftwgl.FtwglAPI;
 import de.gost0r.pickupbot.pickup.Country;
 import de.gost0r.pickupbot.pickup.PickupBot;
-
+import io.github.cdimascio.dotenv.Dotenv;
 import io.sentry.Sentry;
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.util.Locale;
+import java.util.logging.*;
 
 public class PickupBotDiscordMain {
 
     private final static Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
-	private static String env;
-	private static Dotenv dotenv;
+    private static String env;
+    private static Dotenv dotenv;
 
-	public static void main(String[] args) {
-		Locale.setDefault(new Locale("en", "EN"));
-		try {
-			dotenv = Dotenv.configure().filename(".prod.env").load();
-			env = "prod";
-			if (args.length > 0 && args[0].equals("dev")){
-				dotenv = Dotenv.configure().filename(".dev.env").load();
-				env = "dev";
-			}
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.ENGLISH);
+        try {
+            if (args.length > 0 && args[0].equals("dev")) {
+                dotenv = Dotenv.configure().filename(".dev.env").load();
+                env = "dev";
+            } else {
+                dotenv = Dotenv.configure().filename(".prod.env").load();
+                env = "prod";
+            }
 
-			setupLogger();
-			
-			Country.initCountryCodes();
+            setupLogger();
 
-			DiscordBot.setToken(dotenv.get("DISCORD_TOKEN"));
-			DiscordBot.setApplicationId(dotenv.get("DISCORD_APPLICATION_ID"));
-			FtwglAPI.setupCredentials(dotenv.get("FTW_URL"), dotenv.get("FTW_KEY"));
-			PickupBot bot = new PickupBot();
-			bot.init(env);
+            Country.initCountryCodes();
+
+            DiscordBot.setToken(dotenv.get("DISCORD_TOKEN"));
+            DiscordBot.setApplicationId(dotenv.get("DISCORD_APPLICATION_ID"));
+            FtwglAPI.setupCredentials(dotenv.get("FTW_URL"), dotenv.get("FTW_KEY"));
+            PickupBot bot = new PickupBot();
+            bot.init(env);
 
 
-			while (true) {
-				Thread.sleep(5000);
-			}
+            while (true) {
+                Thread.sleep(5000);
+            }
 
-		} catch (IOException | JSONException | SecurityException | InterruptedException e) {
-			LOGGER.log(Level.WARNING, "Exception: ", e);
-			Sentry.capture(e);
-		}
-	}
-	
-	public static void setupLogger() throws SecurityException, IOException {
+        } catch (IOException | JSONException | SecurityException | InterruptedException e) {
+            LOGGER.log(Level.WARNING, "Exception: ", e);
+            Sentry.capture(e);
+        }
+    }
 
-		System.setProperty("java.util.logging.SimpleFormatter.format", "%1$tF %1$tT %4$s - %2$s(): %5$s%6$s%n");
-		Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
-		logger.setLevel(Level.WARNING);
+    public static void setupLogger() throws SecurityException, IOException {
 
-		logger.setUseParentHandlers(false);
-		ConsoleHandler handler = new ConsoleHandler();
-		handler.setLevel(Level.WARNING);
-		logger.addHandler(handler);
+        System.setProperty("java.util.logging.SimpleFormatter.format", "%1$tF %1$tT %4$s - %2$s(): %5$s%6$s%n");
+        Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+        logger.setLevel(Level.WARNING);
 
-		FileHandler logfile = new FileHandler("bot.log");
-		logfile.setFormatter(new SimpleFormatter());
-		logfile.setLevel(Level.WARNING);
-		logger.addHandler(logfile);
+        logger.setUseParentHandlers(false);
+        ConsoleHandler handler = new ConsoleHandler();
+        handler.setLevel(Level.WARNING);
+        logger.addHandler(handler);
 
-		Sentry.init(dotenv.get("SENTRY_DSN") + "?environment=" + env);
+        FileHandler logfile = new FileHandler("bot.log");
+        logfile.setFormatter(new SimpleFormatter());
+        logfile.setLevel(Level.WARNING);
+        logger.addHandler(logfile);
 
-		LOGGER.severe("Bot started.");
-		Sentry.capture("Bot started");
-	}
+        Sentry.init(dotenv.get("SENTRY_DSN") + "?environment=" + env);
+
+        LOGGER.severe("Bot started.");
+        Sentry.capture("Bot started");
+    }
 }
